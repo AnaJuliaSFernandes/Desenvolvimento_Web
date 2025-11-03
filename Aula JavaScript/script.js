5e6f967f97ff51857a153a272a470de0a008d388
Ana Julia <131393446+AnaJuliaSFernandes@users.noreply.github.com>
2025-11-02 23:43:15 -0300

    Refactor JavaScript functions for mouse and color actions

--- Aula JavaScript/script.js
+++ Aula JavaScript/script.js
@@ -8,4 +8,23 @@ function ocultar(){
 
 function cor(){
     document.getElementById('cor').innerHTML 
+
+}
+function mudacor(){
+    let cor= document.getElementById('cor').style.color
+    if(cor == 'yellow'){
+        document.getElementById('cor').style.color = 'blue'
+    }else{
+        document.getElementById('cor').style.color = 'yellow'
+    }
+    
+}
+function ligar(){
+    let acender = document.getElementById('lamp').src;
+    if(acender.includes("pic_bulboff.gif")){
+        document.getElementById('lamp').src='img/pic_bulbon.gif'
+    }else{
+        document.getElementById('lamp').src='img/pic_bulboff.gif'
+    }
+    
 }
