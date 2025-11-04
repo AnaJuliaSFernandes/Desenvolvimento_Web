8479c3e03d705a0604df17ff5728bdce93bce50b
Ana Julia <131393446+AnaJuliaSFernandes@users.noreply.github.com>
2025-11-03 21:46:36 -0300

    Trabalho sala 0311

---
+++ trabalho0311/script.js
@@ -0,0 +1,17 @@
+const botao1 = document.getElementById(cor1);
+botao1.addEventListener('click', function(){
+    botao1.style.backgroundColor = 'yellow';
+    botao1.style.color = 'blue';
+});
+
+const botao2 = document.getElementById(cor2);
+botao1.addEventListener('click', function(){
+    botao1.style.backgroundColor = 'yellow';
+    botao1.style.color = 'blue';
+});
+
+const botao3 = document.getElementById(cor3);
+botao1.addEventListener('click', function(){
+    botao1.style.backgroundColor = 'yellow';
+    botao1.style.color = 'blue';
+});
